d3d180e51f7d45c37de3058b0dd2cbdcfb1d5e3e
dijiflex <dijiflex@gmail.com>
2020-11-11 11:33:57 +0300

    optimized product of array solution

--- JavasScript/Recursion/productOfArray.js
+++ JavasScript/Recursion/productOfArray.js
@@ -6,10 +6,8 @@ and returns the product of them all
 
 function productOfArray(arr){
     if(arr.length === 0) return 1;
-    let product = 1;
-    product = product * arr[0];
 
-    return product * productOfArray(arr.slice(1));
+    return arr[0] * productOfArray(arr.slice(1));
 }
 
 console.log(productOfArray([1,2,3,10])); 
