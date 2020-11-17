f6a65ff6d865af950f5bd632fe1a9051c7e1921f
dijiflex <dijiflex@gmail.com>
2020-11-17 09:43:46 +0300

    Started lineer search

---
+++ JavasScript/Recursion/flatten.js
@@ -0,0 +1,26 @@
+/*
+
+Write a recursive function calle flatten which accepts an array and returns a new array with all values flattened
+
+function flatten(){
+  // add whatever parameters you deem necessary - good luck!
+}
+
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+*/
+function flatten(oldArr){
+    var newArr = []
+        for(var i = 0; i < oldArr.length; i++){
+          if(Array.isArray(oldArr[i])){
+                newArr = newArr.concat(flatten(oldArr[i]))
+          } else {
+                newArr.push(oldArr[i])
+          }
+    };
+    return newArr;
+  }
+flatten([1, 2, 3, [4, 5] ])
+
