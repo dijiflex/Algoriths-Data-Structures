f6be2d058af9776b711a7dfc224234bb9636a2db
dijiflex <dijiflex@gmail.com>
2020-11-22 21:48:15 +0300

    Created the first part of  merger sort

---
+++ JavasScript/Insertion Sort/insertion.js
@@ -0,0 +1,17 @@
+
+
+function insertionSort(arr){
+    for(let i =1; i < arr.length; i++){
+        let currentVal = arr[i];
+
+        for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+            arr[j + 1] =  arr[j];
+            console.log(arr);
+        }
+        arr[j + 1] = currentVal;
+    }
+
+    return arr;
+}
+
+insertionSort([2,1,9,76,4]);
---
+++ JavasScript/Merge Sort/mergeSortedArray.js
@@ -0,0 +1,32 @@
+
+
+
+function merge(arr1, arr2) {
+    let results = [];
+
+    let i = 0;
+    let j = 0;
+    while (i < arr1.length && j < arr2.length) {
+        if (arr2[j] > arr1[i]) {
+            results.push(arr1[i]);
+            i++;
+        } else {
+            results.push(arr2[j]);
+            j++;
+        }
+    }
+
+    while (i < arr1.length) {
+        results.push(arr1[i]);
+        i++;
+    }
+
+    while (j < arr2.length) {
+        results.push(arr2[j]);
+        j++;
+    }
+
+    return results
+}
+console.log(merge([1, 10, 50], [2, 14, 99, 100]));
+
