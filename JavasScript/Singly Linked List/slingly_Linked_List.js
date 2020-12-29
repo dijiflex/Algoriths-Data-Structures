3713f1115f95433ebca3bb6b89e6c09472614479
dijiflex <dijiflex@gmail.com>
2020-12-29 11:31:24 +0300

    Added insert method to singly linked List

--- JavasScript/Singly Linked List/slingly_Linked_List.js
+++ JavasScript/Singly Linked List/slingly_Linked_List.js
@@ -91,6 +91,26 @@ class SinglyLinkedList{
         }
         return  false;
       }
+
+      insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === this.length) {
+            this.push(val);
+             return true;
+            
+            };
+        if(index === 0) return this.unshift(val);
+        
+        const newNode = new Node(val);
+        const prev = this.get(index - 1);
+        const temp = prev.next;
+        prev.next = newNode;
+        newNode.next = temp;
+
+        this.length++;
+        return true;
+
+    }
 }
 
 
