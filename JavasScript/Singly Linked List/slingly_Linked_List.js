5c958b07b000aa4e526f252df55268c3ba72320f
dijiflex <dijiflex@gmail.com>
2020-12-29 11:04:01 +0300

    Added set method to singlyLinkedList class

--- JavasScript/Singly Linked List/slingly_Linked_List.js
+++ JavasScript/Singly Linked List/slingly_Linked_List.js
@@ -81,9 +81,16 @@ class SinglyLinkedList{
         }
 
         return current;
-
-
     }
+
+    set(index, val){
+        let foundNode =   this.get(index);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        }
+        return  false;
+      }
 }
 
 
