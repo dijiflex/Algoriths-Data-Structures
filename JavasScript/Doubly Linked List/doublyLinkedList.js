330fc14591b3c65cba51beb6f4b264ac1b7ef964
dijiflex <dijiflex@gmail.com>
2021-01-04 20:49:32 +0300

    Added insert function to doubly Linked List

--- JavasScript/Doubly Linked List/doublyLinkedList.js
+++ JavasScript/Doubly Linked List/doublyLinkedList.js
@@ -115,4 +115,20 @@ class DoublyLinkedList {
         }
         return false;
     }
+
+    //INSERT
+    insert(index, val){
+        if(index < 0 || index > this.length) return false;
+        if(index === 0) return !!this.unshift(val);
+        if(index === this.length) return !!this.push(val);
+
+        const newNode = new Node(val);
+        const  beforeNode = this.get(index-1);
+        const afterNode = beforeNode.next;
+        
+        beforeNode.next = newNode, newNode.prev = beforeNode;
+        newNode.next = afterNode, afterNode.prev = newNode;
+        this.length++;
+        return true;
+    }
 }
