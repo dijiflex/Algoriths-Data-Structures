9ce1e1c6709da6f979419cbdb456a34c20821e71
dijiflex <dijiflex@gmail.com>
2021-01-04 16:35:58 +0300

    added shift method to Doubly Linked list

--- JavasScript/Doubly Linked List/doublyLinkedList.js
+++ JavasScript/Doubly Linked List/doublyLinkedList.js
@@ -50,4 +50,19 @@ class DoublyLinkedList{
         return poppedNode;
 
     }
+
+    shift(){
+        if(this.length === 0) return undefined;
+        var oldHead = this.head;
+        if(this.length === 1){
+            this.head = null;
+            this.tail = null;
+        }else{
+            this.head = oldHead.next;
+            this.head.prev = null;
+            oldHead.next = null;
+        }
+        this.length--;
+        return oldHead;
+    }
 }
