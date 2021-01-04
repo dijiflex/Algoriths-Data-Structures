b2cd6888b271098a59a915607e73085fc89d633f
dijiflex <dijiflex@gmail.com>
2021-01-04 16:10:01 +0300

    Added push to the doubly Linked  list

--- JavasScript/Doubly Linked List/doublyLinkedList.js
+++ JavasScript/Doubly Linked List/doublyLinkedList.js
@@ -14,5 +14,20 @@ class DoublyLinkedList{
 
     }
 
-    //PUSH
+     //PUSH
+     push (val){
+        let newNode = new Node(val);
+        if(this.length == 0){
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            newNode.prev = this.tail;
+            this.tail = newNode;
+        }
+
+        this.length++;
+
+        return this;
+    }
 }
