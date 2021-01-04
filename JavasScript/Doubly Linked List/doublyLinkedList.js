ccdffbead7d88b9f5ae4c40272a47838e5320989
dijiflex <dijiflex@gmail.com>
2021-01-04 15:57:47 +0300

    initialized doubly Linked List class

---
+++ JavasScript/Doubly Linked List/doublyLinkedList.js
@@ -0,0 +1,18 @@
+class Node {
+    constructor(val){
+        this.val = val;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class DoublyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+
+    }
+
+    //PUSH
+}
