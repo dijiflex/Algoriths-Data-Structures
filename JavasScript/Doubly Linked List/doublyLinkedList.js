340b3e1ab01b0d7a5a04d68fd0b4db40b78f4730
dijiflex <dijiflex@gmail.com>
2021-01-04 16:37:09 +0300

    Modified the shift method in the doubly Linked list

--- JavasScript/Doubly Linked List/doublyLinkedList.js
+++ JavasScript/Doubly Linked List/doublyLinkedList.js
@@ -53,7 +53,7 @@ class DoublyLinkedList{
 
     shift(){
         if(this.length === 0) return undefined;
-        var oldHead = this.head;
+        const oldHead = this.head;
         if(this.length === 1){
             this.head = null;
             this.tail = null;
