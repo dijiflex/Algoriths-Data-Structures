8ae3cfe979a5c67819648dabdb8b9989ab91192b
dijiflex <dijiflex@gmail.com>
2021-01-07 13:09:10 +0300

    Added find to BST

--- JavasScript/Trees/BST.js
+++ JavasScript/Trees/BST.js
@@ -38,6 +38,25 @@ class BinarySearchTree {
              }
          }
     }
+
+    find(value){
+        if(this.root === null) return false;
+
+        let current = this.root;
+        let found = false;
+
+        while(current && !found ){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current
+    }
 }
 
 let tree = new BinarySearchTree() ;
