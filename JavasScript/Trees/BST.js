1fc1e476145b253b74c09b8c1292bec42bc53efe
dijiflex <dijiflex@gmail.com>
2021-01-07 12:31:30 +0300

    Implemented BST insert solution

---
+++ JavasScript/Trees/BST.js
@@ -0,0 +1,43 @@
+class Node {
+    constructor (value){
+        this.value = value,
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+         const newNode = new Node(value);
+         if(this.root === null){
+             this.root = newNode;
+             return this;
+         } else{
+             let current = this.root;
+             while(true){
+                 if(value === current.value) return undefined;
+                 if(value < current.value){
+                     if(current.left === null){
+                         current.left = newNode;
+                         return this;
+                     }
+                     current = current.left;
+                
+                 } else if(value > current.value){
+                     if(current.right === null){
+                         current.right = newNode;
+                         return this;
+                     }
+                     current = current.right;
+                    
+                 }
+             }
+         }
+    }
+}
+
+let tree = new BinarySearchTree() ;
