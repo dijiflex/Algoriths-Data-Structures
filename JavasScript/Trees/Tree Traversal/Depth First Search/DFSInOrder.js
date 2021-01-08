d8afacd1dd5b55f5b2f295205bde538e4a551f36
dijiflex <dijiflex@gmail.com>
2021-01-08 14:20:37 +0300

    Added DFS inorder traversao

---
+++ JavasScript/Trees/Tree Traversal/Depth First Search/DFSInOrder.js
@@ -0,0 +1,137 @@
+class Node {
+    constructor (value){
+        this.value = value,
+        this.left = null;
+        this.right = null
+    }
+}
+
+class BinarySearchTree {
+    constructor(){
+        this.root = null
+    }
+
+    insert(value){
+         const newNode = new Node(value);
+         if(this.root === null){
+             this.root = newNode;
+             return this;
+         } else{
+             let current = this.root;
+             while(true){
+                 if(value === current.value) return undefined;
+                 if(value < current.value){
+                     if(current.left === null){
+                         current.left = newNode;
+                         return this;
+                     }
+                     current = current.left;
+                
+                 } else if(value > current.value){
+                     if(current.right === null){
+                         current.right = newNode;
+                         return this;
+                     }
+                     current = current.right;
+                    
+                 }
+             }
+         }
+    }
+
+    find(value){
+        if(this.root === null) return false;
+
+        let current = this.root;
+        let found = false;
+
+        while(current && !found ){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right
+            } else {
+                found = true;
+            }
+        }
+        if(!found) return undefined;
+        return current
+    }
+
+    contains(value){
+        if(this.root === null) return false;
+        let current = this.root,
+            found = false;
+        while(current && !found){
+            if(value < current.value){
+                current = current.left;
+            } else if(value > current.value){
+                current = current.right;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    BFS(){
+        const data = [];
+        const queue = [];
+       let  node = this.root
+
+        queue.push(node)
+
+        while(queue.length){
+            node = queue.shift();
+            data.push(node.value);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right)
+        }
+
+        return data;
+    }
+    DFSPreOrder(){
+        const data = [];
+
+        function traverse(node){
+            data.push(node.value);
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+     DFSPostOrder(){
+        const data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            if(node.right) traverse(node.right);
+            data.push(node.value);
+        }
+
+        traverse(this.root);
+        return data;
+    }
+    DFSInOrder(){
+        const data = [];
+
+        function traverse(node){
+            if(node.left) traverse(node.left);
+            data.push(node.value);
+            if(node.right) traverse(node.right); 
+        }
+
+        traverse(this.root);
+        return data;
+    }
+}
+
+let tree = new BinarySearchTree() ;
+tree.insert(10);
+tree.insert(6);
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
