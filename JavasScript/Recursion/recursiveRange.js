0ddc9e3426b7729de738bcc08dc27bad4fea7200
dijiflex <dijiflex@gmail.com>
2021-02-12 13:50:31 +0300

    modified the recursive range implementaton

--- JavasScript/Recursion/recursiveRange.js
+++ JavasScript/Recursion/recursiveRange.js
@@ -1,3 +1,5 @@
+
+
 /*
 Write a function called recursiveRange which accepts a number and adds up all the  numbers from 0
 to the number passed to the function
@@ -7,13 +9,12 @@ to the number passed to the function
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55 
 
-function recursiveRange(num){
-    //base case
+
     function recursiveRange(x){
         if (x === 0 ) return 0;
         return x + recursiveRange(x-1);
      }
    
-}
+
 
 recursiveRange(6)
