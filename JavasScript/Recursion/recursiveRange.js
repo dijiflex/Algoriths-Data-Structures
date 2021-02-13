457cd6783eaf11419423a6a2c52503d1bc2b9e35
dijiflex <dijiflex@gmail.com>
2021-02-13 23:35:29 +0300

    M

--- JavasScript/Recursion/recursiveRange.js
+++ JavasScript/Recursion/recursiveRange.js
@@ -9,7 +9,7 @@ to the number passed to the function
 // recursiveRange(6) // 21
 // recursiveRange(10) // 55 
 
-
+//
     function recursiveRange(x){
         if (x === 0 ) return 0;
         return x + recursiveRange(x-1);
