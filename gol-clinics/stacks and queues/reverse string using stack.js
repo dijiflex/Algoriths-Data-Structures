d18c99d68057b3f09081b4efc0211bc3c2bb3de4
dijiflex <dijiflex@gmail.com>
2021-06-07 15:36:46 +0300

    Modified reverse string implementation

--- gol-clinics/stacks and queues/reverse string using stack.js
+++ gol-clinics/stacks and queues/reverse string using stack.js
@@ -3,15 +3,19 @@
 
 
 const reverseString = (string) => {
-    let stack = [...string];
+    let stack = [];
+    for (let i = 0; i < string.length; i++) {
+        stack.push(string.charAt(i))
+        
+    }
+    
     let reversedString = '';
     
     for (let i = stack.length - 1 ; i >= 0; i--) {
-        console.log(i);
         reversedString += stack[i];
+        
     }
     
     return reversedString;
 }
-
-reverseString('felix');
+console.log(reverseString('felix'));
